1fb909c40896ee01fc84f891a95fca6472e605eb
Daniel Sciortino <dmsp8baller@gmail.com>
2018-02-08 19:59:51 -0500

    argparse
    used arg parse to set up passing of different arguments

--- HueStation/huestation.py
+++ HueStation/huestation.py
@@ -1,10 +1,15 @@
-from darksky import forecast
-import datetime
-from phue import Bridge
 from time import sleep
+import datetime
 from random import *
 import sys
+import argparse
+import textwrap
 
+from phue import Bridge
+from darksky import forecast
+
+# Place holder before working on color transitions for 
+# the weather dictionaries
 x=1
 # Different Dictionaries for all the
 # weather patterns
@@ -31,71 +36,83 @@ def range(start, end, step):
 		yield start
 		start += step
 
-def main():
-	debugflag = False
-	key = ''
-	hue = ''
-	ip = '192.168.1.139'
-	longLat = 36.848889, -76.012381
+key = ''
+hue = ''
+ip = ''
+longLat = 36.848889, -76.012381
 
-	b = Bridge(ip,hue)
+b = Bridge(ip,hue)
+parse = argparse.ArgumentParser(prog='huestation.py',
+formatter_class=argparse.RawDescriptionHelpFormatter,description=textwrap.dedent('''\
+			                   __ _        _   _            
+			  /\  /\_   _  ___/ _\ |_ __ _| |_(_) ___  _ __  
+			 / /_/ / | | |/ _ \ \| __/ _` | __| |/ _ \| '_ \ 
+			/ __  /| |_| |  __/\ \ || (_| | |_| | (_) | | | |
+			\/ /_/  \__,_|\___\__/\__\__,_|\__|_|\___/|_| |_|
+			This application will read weather from dark sky api
+			And output it onto a hue light
+			A debug mode is developed for testing
+			It will take the same name of the dictionary
+			This is used for testing certain color patterns
+			
 
-	if(len(sys.argv) > 1):
-		if(sys.argv[1] == '-d'):
-			import logging
-			logging.basicConfig()
-			debugflag = True
-			print(sys.argv[2])
-			print('debugs')
+			Powered by Dark Sky
+			'''))
+group = parse.add_mutually_exclusive_group(required=False)
+group.add_argument('-d',
+					action='store_true',
+                    default=False,
+                    dest='debugFlag',
+                    help="Enables Debugging mode with extra logging, also "
+                    "enables the end user to manually play a scene")
 
-		elif(sys.argv[1] == '-r'):
-			# randomize the state of the light
-			# b.set_light(output,'bri',randint(1,254))
-			b.set_light(output,'hue',randint(1,65535))
-			b.set_light(output,'sat',randint(1,254))
+group.add_argument('-r',
+					action='store_true',
+                    default=False,
+                    dest='randomize',
+                    help='Randomize the color of the designated light for huestation') 
+args = parse.parse_args()
 
-		elif(sys.argv[1] == '-i'):
-			print("                   __ _        _   _             ")
-			print("  /\  /\_   _  ___/ _\ |_ __ _| |_(_) ___  _ __  ")
-			print("	/ /_/ / | | |/ _ \ \| __/ _` | __| |/ _ \| '_ \ ")
-			print("/ __  /| |_| |  __/\ \ || (_| | |_| | (_) | | | |")
-			print("\/ /_/  \__,_|\___\__/\__\__,_|\__|_|\___/|_| |_|")                           
-			print('This application will read weather from dark sky api')
-			print('And output it onto a hue light\n')
-			print('A debug mode is developed for testing')
-			print('It will take the same name of the dictionary')
-			print('This is used for tetsing certain color patterns')
-			print('\n\n\nPowered by Dark Sky')
+if(args.debugFlag):
+	import logging
+	logging.basicConfig()
+	print('debugs')
 
-	# Checks to see if the time is between 9am and 8pm 
-	# else print a message
-	if(datetime.datetime.now().hour in range (9,20,1) == True or debugflag == True):
-		with forecast(key, *longLat) as longLat:
-			upcommingSummary = longLat.hourly[1].summary
-			print(upcommingSummary)
+elif(args.randomize):
+	# randomize the state of the light
+	# b.set_light(output,'bri',randint(1,254))
+	b.set_light(output,'hue',randint(1,65535))
+	b.set_light(output,'sat',randint(1,254))
+	exit()
 
-			if(b.get_light(output,'on')== False):
-				print('Turning light on\n')
-				b.set_light(output,'on',True)
-				b.set_light(output,'bri',127)
+# Checks to see if the time is between 9am and 8pm 
+# else print a message
+if(datetime.datetime.now().hour in range (9,20,1) or 
+	args.debugFlag == True):
+	with forecast(key, *longLat) as longLat:
+		upcommingSummary = longLat.hourly[1].summary
+		print(upcommingSummary)
 
-			if (upcommingSummary.find('Clear') != -1 or sys.argv[2] == 'clear'):
-				b.set_light(output,'ct',clear['ct'],transitiontime=10)
-			elif (upcommingSummary.find('Cloudy') != -1 or sys.argv[2] == 'cloudy'):	
-				b.set_light(output,'bri',partlyCloudy['bri'],transitiontime=50)
-				b.set_light(output,'hue',partlyCloudy['hue'],transitiontime=50)
+		if(b.get_light(output,'on')== False):
+			print('Turning light on\n')
+			b.set_light(output,'on',True)
+			b.set_light(output,'bri',127)
+
+		if (upcommingSummary.find('Clear') != -1 or sys.argv[2] == 'clear'):
+			b.set_light(output,'ct',clear['ct'],transitiontime=10)
+		elif (upcommingSummary.find('Cloudy') != -1 or sys.argv[2] == 'cloudy'):	
+			b.set_light(output,'bri',partlyCloudy['bri'],transitiontime=50)
+			b.set_light(output,'hue',partlyCloudy['hue'],transitiontime=50)
 
 
-			# for i in range(0,65535,1000):
-			# 	b.set_light(output,'hue',i)
-			# 	b.set_light(output,'sat',254)
-			# 	print(i)
-			# 	sleep(.3)  	
-	else:
-		exit()
+		# for i in range(0,65535,1000):
+		# 	b.set_light(output,'hue',i)
+		# 	b.set_light(output,'sat',254)
+		# 	print(i)
+		# 	sleep(.3)  	
+else:
+	exit()
 
-if __name__ == "__main__":
-	main() 
 # Ranges
 # hue 0...65535
 # sat 0...254 
